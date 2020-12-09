1fce0339bfd3bade75b49916c1afa61980c573c0
He Li <hli25@nd.edu>
2020-12-08 22:41:16 -0500

    add spatial_v2 and remove startup.m

--- startup.m
+++
@@ -1,6 +0,0 @@
-% When Matlab starts up, it looks for a file called startup.m in the
-% directory (folder) in which it is being started.  If such a file is
-% found, then it is automatically executed as part of Matlab's start-up
-
-% add PATH_TO_YOUR_spatial_v2
-addpath( genpath( 'C:\Users\hli25\Box\My Research\Robotics\HW3_spatial_v2' ) );
