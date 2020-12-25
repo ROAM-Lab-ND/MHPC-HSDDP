32f3ae3645918e3909f28576032515f3b6695141
He Li <hli25@nd.edu>
2020-12-25 10:20:14 -0500

    minor change

--- Examples/JumpOverGap/GapJumping.m
+++ Examples/JumpOverGap/GapJumping.m
@@ -144,7 +144,7 @@ graphicsOptions.leg_active = 1;
 graphicsOptions.push_active = 0;
 graphicsOptions.GRF_acitive = 0;
 graphicsOptions.gapLoc = 0.96;
-graphicsOptions.gapWidth = 0.3;
+graphicsOptions.gapWidth = 0.4;
 
 graphics = Graphics(get3DMCParams(), WBMC2D);
 % graphics.process2DData(X);
